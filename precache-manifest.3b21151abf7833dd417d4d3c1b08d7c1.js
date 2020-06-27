61ac2751b6710a30540a866e357e5aafadfa7dec
iamrobin-dev <robin.x85@gmail.com>
2020-06-27 23:55:49 +0200

    Updates

---
+++ precache-manifest.3b21151abf7833dd417d4d3c1b08d7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd65ac8b50959690be63ac41f5eec2d",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "eef5369b8572553538d8",
+    "url": "/react-todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "eef5369b8572553538d8",
+    "url": "/react-todo/static/js/2.cb214848.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-todo/static/js/2.cb214848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661f14c56c8d5826bda3",
+    "url": "/react-todo/static/js/main.5ee7371f.chunk.js"
+  },
+  {
+    "revision": "1c15613056b13a1bce12",
+    "url": "/react-todo/static/js/runtime-main.812322bd.js"
+  }
+]);

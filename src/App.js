97a7f7ae7721d504a054ef12637f29deba63a675
iamrobin-dev <robin.x85@gmail.com>
2020-06-27 23:20:25 +0200

    mvp todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 import React from "react";
-import { Container, Button } from "reactstrap";
+import {
+  Container,
+  Button,
+  Form,
+  FormGroup,
+  InputGroup,
+  Input,
+  InputGroupAddon,
+  ListGroup,
+  ListGroupItem,
+} from "reactstrap";
+
 const App = () => {
+  const [value, setValue] = React.useState("");
+  const [tasks, setTasks] = React.useState(["task1", "task2"]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addToDoList(value);
+    setValue("");
+  };
+  const addToDoList = (newTask) => {
+    if (newTask) {
+      console.log(newTask);
+      const updatedTodoList = [...tasks, newTask];
+      console.log(updatedTodoList);
+      setTasks(updatedTodoList);
+    }
+  };
+
+  const handleDelete = (t) => {
+    console.log("deleted");
+    const updateTaskList = tasks.filter((task) => task !== t);
+    console.log(updateTaskList);
+    setTasks(updateTaskList);
+  };
   return (
-    <Container className="mt-4 text-center">
+    <Container className="mt-4">
       <h2 className="text-danger">React Todo-App</h2>
       <hr color="danger" />
+      <Form className="text-center" onSubmit={handleSubmit}>
+        <FormGroup>
+          <InputGroup>
+            <Input
+              placeholder="e.x Learn React"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <InputGroupAddon addonType="append">
+              <Button color="info">Add to Todo's</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </FormGroup>{" "}
+      </Form>
+
+      <ListGroup>
+        {tasks.map((task, index) => (
+          <ListGroupItem color="success" key={index}>
+            {task}- <Button color="success">Completed</Button> |{" "}
+            <Button color="danger" onClick={() => handleDelete(task)}>
+              Delete
+            </Button>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
     </Container>
   );
 };
